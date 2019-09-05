17946cc06162982def2d80bd53391d24380805ad
jhanavi1234 <54845617+jhanavi1234@users.noreply.github.com>
2019-09-05 12:11:57 +0530

    Add files via upload

---
+++ BDCRunner.java
@@ -0,0 +1,10 @@
+package mybdcrunner;
+
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(features="features01",glue="bdcstepdefinitions",tags= {"@Sanity,@Regression"})
+//@CucumberOptions(features="features01",glue="bdcstepdefinitions",tags= {"@Smoke"})
+@CucumberOptions(features="C:\\Users\\j.bysani.shekar\\java1\\MyDemo_01_Cucumber\\features01\\f1demo.feature",glue="bdcstepdefinitions",plugin= {"pretty","html:target/cucumber-html-report","json:target/JanDemo-JSON-report"})
+public class BDCRunner {
+
+}
